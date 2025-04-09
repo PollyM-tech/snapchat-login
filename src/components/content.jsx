8b311362516b6e5525f853e462ce2b5c6bacc6bd
Pauline Moraa <pmoraa431@gmail.com>
2025-04-09 15:25:27 +0300

    added css styling

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 
-
 export default function BodyContent() {
-  return (
-    <div className='container'>
-        <img src="/peps.png" alt="background" className='background-image' />
-      <img src="/snappic.png" alt="more snapchat" className='slogan'/>
-      <footer></footer>
-    </div>
-  )
-}
+    return (
+      <div className='container'>
+        <div className='left-section'>
+          {/* Login section - to be implemented later */}
+        </div>
+  
+        <div className='right-section'>
+            <div className='scroll-area'>
+                <div className='hero-box'>
+                <img src="/snappic.png" alt="Slogan" className='slogan-image' />
+                <img src="/peps.png" alt="People collage" className='people-image' />
+                </div>
+                <div className='middle-content'></div>
+                <div className='footer-placeholder'></div>
+            </div>
+        </div>
+
+
+      </div>
+    )
+  }
+  
+  
+  
+
+
