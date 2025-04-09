e375524344548a7284ca440d199a143e5c3f3447
Pauline Moraa <pmoraa431@gmail.com>
2025-04-09 14:14:57 +0300

    added images

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import BodyContent from './components/content'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
     <>
      <div>
       {/*!! login goes here */}
+      <BodyContent/>
      </div>
     </>
   )
---
+++ src/components/content.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export default function BodyContent() {
+  return (
+    <div className='container'>
+        <img src="/peps.png" alt="background" className='background-image' />
+      <img src="/snappic.png" alt="more snapchat" className='slogan'/>
+      <footer></footer>
+    </div>
+  )
+}
